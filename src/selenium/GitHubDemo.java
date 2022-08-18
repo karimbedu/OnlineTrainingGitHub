e4b7105c0e54ad015abf4242d995f75fa50ce785
karimbedu <kpurushotham.777@gmail.com>
2022-08-18 13:39:21 +0530

    Class added in Selenium Package

---
+++ src/selenium/GitHubDemo.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class GitHubDemo {
+  @Test
+  public void f() {
+	  System.out.println("Hello World");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
